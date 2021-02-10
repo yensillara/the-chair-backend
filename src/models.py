38fd01ea58e4cbece6f0b5d40b6a605a0b528f1e
yensillara <arquiperiodista@gmail.com>
2021-02-09 21:51:27 -0400

    first commit

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Professional(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
