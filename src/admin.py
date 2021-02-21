451c17e9dd975373b6435bc2bb51be2600744aab
ana <anayin1@hotmail.com>
2021-02-21 15:48:55 -0400

    Importacion de clases
    Importacion de clases en admin.py

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, Professional, Client
+from models import db, Professional, Client, Workspace, ProjectWorkSpace
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,8 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(Professional, db.session))
     admin.add_view(ModelView(Client, db.session))
+    admin.add_view(ModelView(Workspace, db.session))
+    admin.add_view(ModelView(ProjectWorkSpace, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
